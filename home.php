291096a6d62a17248346302e465fd45cdf813918
jaoharil <108503847+jaoharil@users.noreply.github.com>
2022-09-18 22:30:47 -0700

    menambahkan tampilan home.txt

---
+++ home.php
@@ -0,0 +1,59 @@
+<div class="align-center">
+  <section class="jumbotron bg-info text-center ">
+    <div class="container">
+      <h2 class="text-light">SISTEM INFORMASI AKADEMIK</h2>
+      <br>
+      <br>
+
+    </div>
+
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="align-center">
+          <div class="card text-info bg-secondary  mb-3">
+            <div class="card-header bg-light">
+              <span>
+                <i class="fas fa-user-tie fa-3x mr-2 "></i> <span class="mr-3 font-weight-normal">
+                  MAHASISWA</span><br>
+              </span>
+            </div>
+            <h6 class="m-2"> <a href="?page=siswa" class="text-light"><i class="fa fa-arrow-circle-right mr-2"></i>Lihat
+                Lihat Selengkapnya</a> </h6>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div calss="align-center">
+          <div class="card text-info bg-secondary mb-3 ">
+            <div class=" card-header bg-light">
+              <span>
+                <i class="fas fa-user-graduate fa-3x mr-2"></i> <span class="mr-3">DATA GURU</span>
+              </span>
+            </div>
+            <h6 class="m-2"> <a href="?page=guru" class="text-light"><i class="fa fa-arrow-circle-right mr-2"></i>Lihat Selengkapnya</a> </h6>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="card text-info bg-secondary mb-3">
+          <div class="card-header bg-light">
+            <span>
+              <i class="fas fa-book fa-3x mr-2"></i> <span class="mr-3">DATA MAPEL</span>
+            </span>
+          </div>
+          <h6 class="m-2"> <a href="?page=mapel" class="text-light"><i class="fa fa-arrow-circle-right mr-2"></i>Lihat Selengkapnya</a> </h6>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="card text-info bg-secondary mb-3">
+          <div class="card-header bg-light">
+            <span>
+              <i class="fas fa-graduation-cap fa-3x mr-2"></i> <span class="mr-3">DATA JURUSAN</span>
+            </span>
+          </div>
+          <h6 class="m-2"> <a href="?page=jurusan" class="text-light"><i class="fa fa-arrow-circle-right mr-2"></i>Lihat Selengkapnya</a> </h6>
+        </div>
+      </div>
+  </section>
+</div>
